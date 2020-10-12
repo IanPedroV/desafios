09b6a4fd8107d335d86a01b3df78fe5b97ab2991
IanPedroV <ian0198@gmail.com>
2020-10-12 17:18:36 -0300

    feat: adding docker support

--- crawlers/main.py
+++ crawlers/main.py
@@ -1,9 +1,14 @@
+import sys
+
 from reddit_scrapper import process_input, print_subreddits_and_threads
 from telegram_bot import start_bot
 
 
 def start():
-    mode = input("Do you want to input the subreddits or call me on me telegram?. Type 1 for local CLI of 2 for "
+    if len(sys.argv) == 2:
+        mode = sys.argv[1]
+    else:
+        mode = input("Do you want to input the subreddits or call me on me telegram?. Type 1 for local CLI of 2 for "
                  "telegram")
     if mode == "1":
         reddit_input = input('please input the subreddits as it follows: a;b;c')
@@ -11,6 +16,8 @@ def start():
         print_subreddits_and_threads(subreddits)
     elif mode == "2":
         start_bot()
+    else:
+        print("INVALID MODE! Type 1 or 2.")
 
 
 if __name__ == '__main__':
--- crawlers/telegram_bot.py
+++ crawlers/telegram_bot.py
@@ -1,34 +1,44 @@
 import os
 
-import telebot
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-from reddit_scrapper import print_subreddits_and_threads, process_input, has_error
+from reddit_scrapper import has_error, print_subreddits_and_threads, process_input
 from dotenv import load_dotenv
 
 load_dotenv()
 
-bot = telebot.TeleBot(os.getenv('TELEGRAM_TOKEN'))
+
+def listen_for_subreddits(update, context):
+    chat_id = update.message.chat_id
+    subreddits = process_input(context.args[0])
+    print_subreddits_and_threads(subreddits)
+    for subreddit in subreddits:
+        if has_error(subreddit):
+            context.bot.send_message(chat_id, '\nError occurred when trying to scrap, message: ' + subreddit.error)
+            break
+
+        for thread in subreddit:
+            context.bot.send_message(chat_id,
+                                     'subreddit: ' + thread.subreddit + '\n' + 'title: ' + thread.title + '\n' + 'author: '
+                                     + thread.author + '\n' + 'link: ' + thread.link + '\n' + 'link comments: '
+                                     + thread.link_comment + '\n' + 'up_votes: ' + str(thread.up_votes))
+
+
+def start(update, context):
+    chat_id = update.message.chat_id
+    context.bot.send_message(chat_id, 'Type /NadaPraFazer subrreditA;subrreditB;subrreditC')
+
+
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command. Try /start")
 
 
 def start_bot():
-    @bot.message_handler(commands=['start', 'help'])
-    def send_welcome(message):
-        # change this
-        bot.reply_to(message, "Olá, bem-vindo ao bot!")
-
-    @bot.message_handler(commands=['NadaPraFazer'])
-    def listen_for_subreddits(message):
-        subreddits = process_input(message.text.replace('/NadaPraFazer', ''))
-        print_subreddits_and_threads(subreddits)
-        for subreddit in subreddits:
-            if has_error(subreddit):
-                bot.send_message(message.chat.id, '\nError occurred when trying to scrap, message: ' + subreddit.error)
-                break
-            for thread in subreddit:
-                bot.send_message(message.chat.id,
-                                 'subreddit: ' + thread.subreddit + '\n' + 'title: ' + thread.title + '\n' + 'author: '
-                                 + thread.author + '\n' + 'link: ' + thread.link + '\n' + 'link comments: '
-                                 + thread.link_comment + '\n' + 'up_votes: ' + str(thread.up_votes))
-
+    updater = Updater(os.getenv('TELEGRAM_TOKEN'))
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('NadaPraFazer', listen_for_subreddits))
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(MessageHandler(Filters.command, unknown))
+    updater.start_polling()
     print("Reddit Concierge successfully started!")
-    bot.polling()
+    updater.idle()
